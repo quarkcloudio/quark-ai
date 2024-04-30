f18dbad639112bdaa27da3ae77a62a26eec5de36
tangtanglove <dai_hang_love@126.com>
2024-04-30 19:58:58 +0800

    chore: wip

--- ai/ocr/ocr.py
+++ ai/ocr/ocr.py
@@ -4,9 +4,7 @@ from modelscope.utils.constant import Tasks
 class OCR:
 
     # OCR识别
-    def getImageText():
-        ocr_recognition = pipeline(Tasks.ocr_recognition, model='damo/cv_convnextTiny_ocr-recognition-general_damo')
+    def get_image_text(img_url):
+        ocr_recognition = pipeline(Tasks.ocr_recognition, model='damo/cv_convnextTiny_ocr-recognition-scene_damo')
 
-        ### 使用url
-        img_url = 'http://duguang-labelling.oss-cn-shanghai.aliyuncs.com/mass_img_tmp_20220922/ocr_recognition.jpg'
         return ocr_recognition(img_url)
--- api/ocr.py
+++ api/ocr.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,request
 from ai.ocr.ocr import OCR
 
 # 创建蓝图ocr_bp
@@ -7,4 +7,8 @@ ocr_bp = Blueprint('ocr', __name__)
 # 定义路由
 @ocr_bp.route('/api/ocr', methods=['GET', 'POST'])
 def ocr():
-    return OCR.getImageText()
+    url = request.args.get('url', '')
+    if url == '':
+        return 'url is empty'
+
+    return OCR.get_image_text(url)
